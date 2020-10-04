381267a1213ba4b66d0fe212c8175b6df852c102
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-10-04 15:23:30 +0530

    Created Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <nav class="footer__body navbar fixed-bottom navbar-light bg-light">
+      <div class="w-100 py-2 text-center">
+        Made with <span>❤️</span> and <span>☕</span> by{" "}
+        <a href="https://github.com/harsh2124">Harsh Patel</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Footer;
