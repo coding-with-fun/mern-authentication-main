085cad896474d9a63f83f9820fb5bd5dfb841a28
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-10-04 20:28:47 +0530

    Styling tweaks of Sign Up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -9,37 +9,39 @@ export default class SignUp extends Component {
           <img src={logo} alt="Sign Up" />
         </div>
         <div className="main__form">
-          <p>Create new account</p>
-          <input
-            type="text"
-            name="full_name"
-            id="full_name"
-            placeholder="Name"
-            className="mb-3"
-            autoFocus
-          />
-          <input
-            type="email"
-            name="email_id"
-            id="email_id"
-            placeholder="Email address"
-            className="mb-3"
-          />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="mb-3"
-          />
-          <input
-            type="password"
-            name="confirm_password"
-            id="confirm_password"
-            placeholder="Confirm Password"
-            className="mb-3"
-          />
-          <div className="submit__button">Sign In</div>
+          <div className="form_fields">
+            <p>Create new account</p>
+            <input
+              type="text"
+              name="full_name"
+              id="full_name"
+              placeholder="Name"
+              className="mb-3"
+              autoFocus
+            />
+            <input
+              type="email"
+              name="email_id"
+              id="email_id"
+              placeholder="Email address"
+              className="mb-3"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="mb-3"
+            />
+            <input
+              type="password"
+              name="confirm_password"
+              id="confirm_password"
+              placeholder="Confirm Password"
+              className="mb-3"
+            />
+            <div className="submit__button">Sign In</div>
+          </div>
         </div>
       </div>
     );
